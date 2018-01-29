ba7a4bcffe9cd3e3a6cc5614c9d7bc145090d19f
Unknown <by.darklancer@gmail.com>
2018-01-29 15:13:49 -0300

    feat: componente "lista-cursos"
    Se crea el nuevo componente "lista-cursos"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListaCursosComponent } from './lista-cursos/lista-cursos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaCursosComponent
   ],
   imports: [
     BrowserModule
