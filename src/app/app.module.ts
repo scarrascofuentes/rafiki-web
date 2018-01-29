a41bc4c047b715e9f75f1768e01fc22980e3bd67
Unknown <ogasvega.israel@gmail.com>
2018-01-29 18:33:53 -0300

    feat: Componente y Servicio de Organizacion
    Se agregó el nuevo componente, servicio y la funcionalidad 'Obtener lista de las Organizaciones'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+//Servicios
+import { OrganizacionService } from './services/organizacion.service';
 
 import { AppComponent } from './app.component';
+import { ListaOrganizacionesComponent } from './lista-organizaciones/lista-organizaciones.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaOrganizacionesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [OrganizacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
