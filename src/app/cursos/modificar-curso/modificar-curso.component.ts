33daca2f0f265cc39c552b0c1c70840a46c1b5e8
Unknown <by.darklancer@gmail.com>
2018-02-01 10:15:20 -0300

    feat: funcion put agregada

--- src/app/cursos/modificar-curso/modificar-curso.component.ts
+++ src/app/cursos/modificar-curso/modificar-curso.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-modificar-curso',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificarCursoComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  idCurso: String = '';
+  nivel: String = '';
+  asignatura: String = '';
+  profesorJefe: String = '';
+  salaCurso: String = '';
+  totalAlumnos: Number;
+  domain: string = 'http://localhost:10010';
+  curso: any;
+  id: String;
+
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private HttpClient: HttpClient) {
+    this.rForm = formBuilder.group({
+      'idCurso': [null, Validators.required],
+      'nivel': [null, Validators.required],
+      'asignatura': [null, Validators.required],
+      'profesorJefe': [null, Validators.required],
+      'salaCurso': [null, Validators.required],
+      'totalAlumnos': [null, Validators.required],
+      // 'validate': ''
+    });
+    this.route.params.subscribe(res => {
+      this.id = res.id;
+      console.log(res.id)
+    })
+  }
+
+  updatePut(put) {
+    console.log(put);
+    this.HttpClient.put(`${this.domain}/cursos/${this.id}`, put).subscribe(data => {
+      console.log(put);
+    });
+  }
 
   ngOnInit() {
+    this.HttpClient.get(`${this.domain}/cursos/${this.id}`).subscribe(data => {
+      // console.log(data);
+      this.curso = data;
+      console.log(this.curso);
+    });
   }
 
 }
