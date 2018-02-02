3b6913f48efa4118fe0cd4093999d59417f007dc
Johann <johann.aevt@gmail.com>
2018-02-02 11:08:26 -0300

    style: Separando services y components en modulos
    Agrupamos los componentes y servicios en 3 modulos distintos, estos son: 
    - Curso
    - AuthenticationAuthorization
    - Organizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,94 +1,31 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-//Animations
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-//Forms
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-
-//Servicios
-import { OrganizacionService } from './organizacion/services/organizacion.service';
-
-import { ListaOrganizacionesComponent } from './organizacion/lista-organizaciones/lista-organizaciones.component';
-import { CrearOrganizacionComponent } from './organizacion/crear-organizacion/crear-organizacion.component';
-import { ModificarOrganizacionComponent } from './organizacion/modificar-organizacion/modificar-organizacion.component';
-import { DetallesOrganizacionComponent } from './organizacion/detalles-organizacion/detalles-organizacion.component';
-import { AppRoutingModule } from './/app-routing.module';
-
 import { HttpModule } from '@angular/http'
-
-import { AppComponent } from './app.component';
-import { ListaCursosComponent } from './lista-cursos/lista-cursos.component';
-import { CrearCursoComponent } from './cursos/crear-curso/crear-curso.component';
-import { ModificarCursoComponent } from './cursos/modificar-curso/modificar-curso.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DetalleCursoComponent } from './cursos/detalle-curso/detalle-curso.component';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers/index';
+import { OrganizacionModule } from './organizacion/organizacion.module';
+import { CursoModule } from './curso/curso.module';
 
-import { routing }        from './app.routing';
-
-import { AlertComponent } from './_directives/index';
-import { AuthGuard } from './_guards/index';
-import { JwtInterceptor } from './_helpers/index';
-import { AlertService, AuthenticationService, UserService } from './_services/index';
-import { HomeComponent } from './home/index';
-import { LoginComponent } from './login/index';
-import { RegisterComponent } from './register/index';
-import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListaOrganizacionesComponent,
-    CrearOrganizacionComponent,
-    ModificarOrganizacionComponent,
-    DetallesOrganizacionComponent,
-    ListaCursosComponent,
-    CrearCursoComponent,
-    ModificarCursoComponent,
-    DetalleCursoComponent,
-    AlertComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    ListaCursosComponent,
-    CrearCursoComponent,
-    ModificarCursoComponent,
-    DetalleCursoComponent,
-  EditarUsuarioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpModule,
-    routing
+    OrganizacionModule,
+    CursoModule
   ],
-  providers: [
-    OrganizacionService,
-    AuthGuard,
-    AlertService,
-    AuthenticationService,
-    UserService,
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: JwtInterceptor,
-            multi: true
-        },
-
-        // provider used to create fake backend
-        fakeBackendProvider
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
