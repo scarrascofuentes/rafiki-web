31700b5c3aa05fb4f6dbf34412590d1360f11a8d
Unknown <by.darklancer@gmail.com>
2018-02-02 15:45:54 -0300

    feat: metodos get y delete

--- src/app/asignatura/lista-asignatura/lista-asignatura.component.ts
+++ src/app/asignatura/lista-asignatura/lista-asignatura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-asignatura',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAsignaturaComponent implements OnInit {
 
-  constructor() { }
+  rForm: FormGroup;
+  domain: string = 'http://localhost:10010';
+  asignatura: any;
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {
+    this.rForm = formBuilder.group({
+      'nombre': [null, Validators.required],
+      'horasPedagogicasConJecAnual': [null, Validators.required],
+      'horasPedagogicasSinJecAnual': [null, Validators.required],
+      'horasPedagogicasConJecSemanal': [null, Validators.required],
+      'horasPedagogicasSinJecSemanal': [null, Validators.required]
+    });
+  }
+
+  getAsignatura() {
+    this.httpClient.get(`${this.domain}/asignaturas`).subscribe(data => {
+      // console.log(data);
+      this.asignatura = data;
+      console.log(this.asignatura);
+    });
+  }
+
+  deleteAsignatura(id) {
+    this.httpClient.delete(`${this.domain}/asignaturas`).subscribe(data => {
+    });
+  }
 
   ngOnInit() {
+    this.getAsignatura();
   }
 
 }
