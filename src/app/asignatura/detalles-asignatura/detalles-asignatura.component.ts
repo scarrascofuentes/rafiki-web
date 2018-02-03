fe9fbfb4609b3c4d9c58787d5f5427cfc68fcb47
Unknown <by.darklancer@gmail.com>
2018-02-02 22:52:07 -0300

    feat: componente "detalles-asignatura"

---
+++ src/app/asignatura/detalles-asignatura/detalles-asignatura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-asignatura',
+  templateUrl: './detalles-asignatura.component.html',
+  styleUrls: ['./detalles-asignatura.component.css']
+})
+export class DetallesAsignaturaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
