f69aaaad2a3b57bbdb7950b4be3d7fe28ddf3690
Unknown <by.darklancer@gmail.com>
2018-02-03 20:22:15 -0300

    feat: datos mostrados correctamente

--- src/app/asignatura/detalles-asignatura/detalles-asignatura.component.ts
+++ src/app/asignatura/detalles-asignatura/detalles-asignatura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalles-asignatura',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallesAsignaturaComponent implements OnInit {
 
-  constructor() { }
+  asignatura: any;
+  id: string= "5a763951a100651780fe93de"; //Borrar
+  domain: string = 'http://localhost:10010';
+
+  constructor(private httpClient: HttpClient, private route: ActivatedRoute) {
+    // this.route.params.subscribe(res => {
+    //   this.id = res.id;
+    //   console.log(res.id)
+    // })
+  }
+
+  getAsignatura() {
+    this.httpClient.get(`${this.domain}/asignaturas/${this.id}`).subscribe(data => {
+      //console.log(data);
+      this.asignatura = data;
+      console.log(this.asignatura)
+    });
+  }
 
   ngOnInit() {
+    this.getAsignatura();
   }
 
 }
