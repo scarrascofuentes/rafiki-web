a6ab893dc8887e9842779c9326a0b0e72e1323bb
Unknown <by.darklancer@gmail.com>
2018-02-04 21:30:13 -0300

    feat: animation añadidos

--- src/app/asignatura/lista-asignatura/lista-asignatura.component.ts
+++ src/app/asignatura/lista-asignatura/lista-asignatura.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-lista-asignatura',
   templateUrl: './lista-asignatura.component.html',
-  styleUrls: ['./lista-asignatura.component.css']
+  styleUrls: ['./lista-asignatura.component.css'],
+  animations: [
+    trigger('animation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('200ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 }),
+          ]))]), { optional: true })
+        ,
+        query(':leave', stagger('200ms', [
+          animate('.6s ease-out', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
+          ]))]), { optional: true })
+      ])
+    ])
+  ]
 })
 export class ListaAsignaturaComponent implements OnInit {
 
@@ -31,9 +52,18 @@ export class ListaAsignaturaComponent implements OnInit {
     });
   }
 
-  deleteAsignatura(id) {
+  deleteAsignatura(id,i) {
     this.httpClient.delete(`${this.domain}/asignaturas/${id}`).subscribe(data => {
     });
+    const respuesta = confirm('¿ Estás seguro que deseas eliminarlo?');
+    if (respuesta) {
+      this.httpClient.delete(`${this.domain}/asignaturas/${id}`).subscribe(data => {
+        this.asignaturas.Asignaturas.splice(i, 1)
+      }, (err) => {
+        console.log(err);
+      }
+      )
+    }
   }
 
   ngOnInit() {
