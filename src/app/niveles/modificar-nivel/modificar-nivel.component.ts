5025905f9a8e804797263eccfb156c31469c54c3
Héctor Astorga <hm.astorga@gmail.com>
2018-02-05 01:20:22 -0300

    feat: se agrega nuevo módulo
    modificar-nivel.component

---
+++ src/app/niveles/modificar-nivel/modificar-nivel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-nivel',
+  templateUrl: './modificar-nivel.component.html',
+  styleUrls: ['./modificar-nivel.component.css']
+})
+export class ModificarNivelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
