01980ce39c7328baee22b4323d3f5244dfa27b34
Héctor Astorga <hm.astorga@gmail.com>
2018-02-04 23:51:17 -0300

    feat: importación de rutas de los nuevos módulos de nivel

--- src/app/niveles/nivel.module.ts
+++ src/app/niveles/nivel.module.ts
@@ -5,6 +5,8 @@ import { ListaNivelesComponent } from './lista-niveles/lista-niveles.component';
 
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CrearNivelComponent } from './crear-nivel/crear-nivel.component';
+import { DetalleNivelComponent } from './detalle-nivel/detalle-nivel.component';
 
 @NgModule({
   imports: [
@@ -14,10 +16,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
   ],
   declarations: [
-    ListaNivelesComponent
+    ListaNivelesComponent,
+    CrearNivelComponent,
+    DetalleNivelComponent
   ],
   exports:[
-    ListaNivelesComponent
+    ListaNivelesComponent,
+    CrearNivelComponent
   ]
 })
 export class NivelModule { }
