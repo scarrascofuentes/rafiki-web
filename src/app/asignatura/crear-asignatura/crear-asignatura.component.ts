988f283c2c3d8da4f8306bef6347f7f00b94966f
Unknown <by.darklancer@gmail.com>
2018-02-04 22:31:07 -0300

    feat: funcion post agregado (falta enfasis)
    falta agregar el atributo "enfasis" al "rForm"

--- src/app/asignatura/crear-asignatura/crear-asignatura.component.ts
+++ src/app/asignatura/crear-asignatura/crear-asignatura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-crear-asignatura',
@@ -6,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-asignatura.component.css']
 })
 export class CrearAsignaturaComponent implements OnInit {
+  
+  rForm: FormGroup;
+  domain: string = 'http://localhost:10010';
+  asignatura: any;
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {
+    this.rForm = formBuilder.group({
+      'nombre': [null, Validators.required],
+      'horasPedagogicasConJecAnual': [null, Validators.required],
+      'horasPedagogicasSinJecAnual': [null, Validators.required],
+      'horasPedagogicasConJecSemanal': [null, Validators.required],
+      'horasPedagogicasSinJecSemanal': [null, Validators.required]
+    });
+   }
 
-  constructor() { }
+   addPost(post) {
+    // this.idCurso = post.idCurso;
+    this.httpClient.post(`${this.domain}/asignaturas`, post).subscribe(data=>{
+      console.log(post);
+    });
+  }
 
   ngOnInit() {
   }
